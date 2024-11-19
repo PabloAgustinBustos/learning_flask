e15e4f13aa4d37161f52f060d31f3041d85a50f0
Pablo Bustos <pablobustos609@gmail.com>
2024-11-19 17:32:02 -0300

    add endpoints and templates

--- api.py
+++ api.py
@@ -1,64 +1,70 @@
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
+from flask import Flask, request, render_template, redirect, url_for
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@localhost:5432/pythondb"
-
-db = SQLAlchemy(app)
-
-api = Api(app)
-
-class UserModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(40), unique=True, nullable=False)
-    email = db.Column(db.String(40), unique=True, nullable=False)
-
-    def __repr__(self):
-        return f"User(name = {self.name}, email = {self.email})"
-
-user_args = reqparse.RequestParser()
-user_args.add_argument("name", type=str, required=True, help="Name cannot be blank")
-user_args.add_argument("email", type=str, required=True, help="Email cannot be blank")
-
-# Response's body structure
-userFields = {
-    "id": fields.Integer,
-    "name": fields.String,
-    "email": fields.String
-}
-
-# Router de users
-class Users(Resource):
-    @marshal_with(userFields)
-    def get(self):
-        users = UserModel.query.all()
-        return users
-
-    @marshal_with(userFields)
-    def post(self):
-        args = user_args.parse_args()
-        user = UserModel(name=args["name"], email=args["email"])
-
-        db.session.add(user)
-
-# Bindea la ruta al router
-api.add_resource(Users, "/api/users/")
+app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def home():
-    return "<h1>Flask REST API</h1>"
+    title = "home"
+    owner = "Pablo"
 
-@app.route("/greet/<name>")
+    my_list = [1, 2, 3, 4, 5]
+
+    return render_template("index.html", title=title, owner=owner, my_list=my_list)
+
+@app.route("/page")
+def page():
+    my_list = [1, 2, 3, 4, 5]
+
+    return render_template("page.html", my_list=my_list, some_text="test")
+
+@app.route("/redirect-endpoint")
+def redirect_endpoint():
+    return redirect(url_for("page"))
+
+@app.template_filter("reverse_string")
+def reverse_string(s):
+    return s[::-1]
+
+@app.template_filter("repeat")
+def reverse_string(s, times=1):
+    return s*times
+
+@app.route("/api/user", methods=["GET", "POST", "PUT"])
+def user():
+    users = [
+        {
+            "name": "Pablo",
+            "age": 24
+        },
+
+        {
+            "name": "Juan",
+            "age": 22
+        },
+    ]
+
+    if request.method == "GET":
+        return users
+    elif request.method == "POST":
+        return "<h1>Create User</h1>"
+    else:
+        return "<h1>Errror</h1>", 400
+
+@app.route("/api/greet/<name>")
 def greet(name):
     return f"<h1>Hello {name}</h1>"
 
-@app.route("/add/<number1>/<number2>")
+@app.route("/api/add/<number1>/<number2>")
 def add(number1, number2):
     return f"{number1} + {number2} = {int(number1) + int(number2)}"
 
-@app.route("/filter")
+@app.route("/api/filter")
 def filter():
+    print(request.args)
+
+    if "min" in request.args.keys():
+        pass
+
     return str(request.args)
 
 if __name__ == "__main__":

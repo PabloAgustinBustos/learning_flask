cb6a9f3b6f7bfb98e6d036714b3ae1898256daaa
PabloAgustinBustos <pablobustos609@gmail.com>
2024-11-24 20:06:20 -0300

    add form handling

--- api.py
+++ api.py
@@ -1,71 +1,78 @@
-from flask import Flask, request, render_template, redirect, url_for
+import os
+import uuid
+from flask import Flask, request, render_template, Response, send_from_directory, jsonify
+import pandas as pd
 
 app = Flask(__name__, template_folder="templates")
 
-@app.route("/")
-def home():
-    title = "home"
-    owner = "Pablo"
-
-    my_list = [1, 2, 3, 4, 5]
-
-    return render_template("index.html", title=title, owner=owner, my_list=my_list)
-
-@app.route("/page")
-def page():
-    my_list = [1, 2, 3, 4, 5]
-
-    return render_template("page.html", my_list=my_list, some_text="test")
-
-@app.route("/redirect-endpoint")
-def redirect_endpoint():
-    return redirect(url_for("page"))
-
-@app.template_filter("reverse_string")
-def reverse_string(s):
-    return s[::-1]
-
-@app.template_filter("repeat")
-def reverse_string(s, times=1):
-    return s*times
-
-@app.route("/api/user", methods=["GET", "POST", "PUT"])
-def user():
-    users = [
-        {
-            "name": "Pablo",
-            "age": 24
-        },
-
-        {
-            "name": "Juan",
-            "age": 22
-        },
-    ]
-
+@app.route("/", methods=["GET", "POST"])
+def index():
     if request.method == "GET":
-        return users
-    elif request.method == "POST":
-        return "<h1>Create User</h1>"
-    else:
-        return "<h1>Errror</h1>", 400
+        return render_template("index.html", error=False)
+    elif request.method == "POST":    
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-@app.route("/api/greet/<name>")
-def greet(name):
-    return f"<h1>Hello {name}</h1>"
+        if username == "pablo" and password == "pablo":
+            return "Success"
+        else:
+            return render_template("index.html", error=True)
 
-@app.route("/api/add/<number1>/<number2>")
-def add(number1, number2):
-    return f"{number1} + {number2} = {int(number1) + int(number2)}"
+@app.route("/upload-file", methods=["POST"])
+def upload_file():
+    file = request.files["file"]
 
-@app.route("/api/filter")
-def filter():
-    print(request.args)
+    if file.content_type == "text/plain":
+        return file.read().decode()
+    elif (
+        file.content_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" or
+        file.content_type == ", application/vnd.ms-excel"
+    ):
+        df = pd.read_excel(file)
+        return df.to_html()
 
-    if "min" in request.args.keys():
-        pass
+@app.post("/convert-csv")
+def convert_csv():
+    file = request.files["file"]
 
-    return str(request.args)
+    df = pd.read_excel(file)
+    
+    response = Response(df.to_csv(), mimetype="text/csv", headers={
+        "Content-Disposition": "attachment; filename=result.csv"
+    })
+
+    return response
+
+@app.post("/convert-csv2")
+def convert_csv_2():
+    file = request.files["file"]
+    
+    df = pd.read_excel(file)
+
+    if not os.path.exists("downloads"):
+        os.makedirs("downloads")
+    
+    filename = f"{uuid.uuid4()}.csv"
+
+    df.to_csv(os.path.join("downloads", filename))
+
+    return render_template("download.html", filename=filename)
+
+@app.route("/download/<filename>")
+def download(filename):
+    return send_from_directory("downloads", filename, download_name="result")
+
+@app.post("/handle-post")
+def handle_post():
+    greeting = request.json["greeting"]
+    name = request.json["name"]
+
+    with open("file.txt", "w") as f:
+        f.write(f"{greeting} {name}")
+    
+    return {
+        "message": "Written!"
+    }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
